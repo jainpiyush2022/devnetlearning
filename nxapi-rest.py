4e0ba5eec470fd5cf82b82d9c129cd1f202a137f
Piyush Jain <piyujain20222@gmail.com>
2020-04-10 02:57:05 +0530

    updated comments on NXAPI REST file

--- nxapi-rest.py
+++ nxapi-rest.py
@@ -1,7 +1,9 @@
+# This program uses the NXAPI REST to update the configuration on an interface.
 import requests
 import json
 from pprint import pprint
 
+# Login URL
 url = "https://sbx-nxos-mgmt.cisco.com/api/aaaLogin.json"
 
 payload = "{\n\"aaaUser\": {\n\"attributes\": {\n\"name\": \"admin\",\n\"pwd\": \"Admin_1234!\"\n}\n}\n}"
@@ -10,6 +12,7 @@ headers = {
 
 response = requests.post(url, headers=headers, data = payload,verify=False).json()
 
+# Extract the token as it will be used to run the PUT REST API.
 token = response['imdata'][0]['aaaLogin']['attributes']['token']
 
 #print(token)
@@ -17,6 +20,7 @@ token = response['imdata'][0]['aaaLogin']['attributes']['token']
 cookies = {}
 cookies['APIC-cookie'] = token
 
+# API URL to update the configuration of physical interface eth 1/97
 url = "https://sbx-nxos-mgmt.cisco.com/api/node/mo/sys/intf/phys-[eth1/97].json"
 
 payload = "{\n    \"l1PhysIf\": {\n          \t\"attributes\": {\n          \t\t\"descr\": \" \"\n          \t}\n    }\n}"
@@ -26,6 +30,7 @@ headers = {
 
 response_put = requests.put(url, headers=headers, data = payload,cookies=cookies,verify=False)
 
+# Status code 200 ok means good reponse from device.
 if (response_put.status_code == 200):
     print("Successfully updated the configuration!")
 
